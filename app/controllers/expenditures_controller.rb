54d0a8e9666662c016d0237a4e2960212b19681d
thanhdat285 <dd.tt97@gmail.com>
2017-02-12 00:31:32 +0700

    Create new expenditure

--- app/controllers/expenditures_controller.rb
+++ app/controllers/expenditures_controller.rb
@@ -1,27 +1,32 @@
 class ExpendituresController < ApplicationController
   before_action :login_required
+  before_action :enough_params, only: :update_all
 
   def index
     @expenditures = current_user.expenditures.order(:day)
   end
 
-  def new
-  end
-
   def create
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def delete
+    day = [params["day_1i"], params["day_2i"], params["day_3i"]].join("/")
+    @expenditure = current_user.expenditures.new(
+      day: day, name: params["name"], money: params["money"]
+    )
+    if @expenditure.save
+      flash[:success] = "Create new expenditure success"
+    else
+      flash[:error] = @expenditure.errors
+    end
+    redirect_to expenditures_path
   end
 
   private
   def expenditure_params
     params.require(:expenditure).permit(:name, :money, :day)
+  end
+
+  def enough_params
+    if ["day_1i", "day_2i", "day_3i", "name", "money"].any? {|k| params[k].nil?}
+      redirect_to root_path
+    end
   end
 end
